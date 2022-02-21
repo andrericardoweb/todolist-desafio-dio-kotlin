e3d42488f32b4860e2fd7c839d20c9fffbc5db6b
andrericardoweb <andrericardoweb@gmail.com>
2022-02-21 13:01:27 -0300

    edit and delete task, empty state

--- app/src/main/java/me/dio/todolist_desafiodio/ui/MainActivity.kt
+++ app/src/main/java/me/dio/todolist_desafiodio/ui/MainActivity.kt
@@ -1,9 +1,11 @@
 package me.dio.todolist_desafiodio.ui
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import me.dio.todolist_desafiodio.databinding.ActivityMainBinding
 import me.dio.todolist_desafiodio.datasource.TaskDataSource
 
@@ -17,6 +19,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.rvTasks.adapter = adapter
+        updateList()
+
         insertListeners()
     }
 
@@ -26,21 +31,28 @@ class MainActivity : AppCompatActivity() {
         }
 
         adapter.listenerEdit = {
-            Log.e("TAG", "listenerEdit: $it")
+            val intent = Intent(this, AddTaskActivity::class.java)
+            intent.putExtra(AddTaskActivity.TASK_ID, it.id)
+            startActivityForResult(intent, CREATE_NEW_TASK)
         }
 
         adapter.listenerDelete = {
-            Log.e("TAG", "listenerDelete: $it")
-
+            TaskDataSource.deleteTask(it)
+            updateList()
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == CREATE_NEW_TASK) {
-            binding.rvTasks.adapter = adapter
-            adapter.submitList(TaskDataSource.getList())
-        }
+        if (requestCode == CREATE_NEW_TASK && resultCode == Activity.RESULT_OK) updateList()
+    }
+
+    private fun updateList() {
+        val list = TaskDataSource.getList()
+        binding.includeEmptyState.emptyState.visibility = if (list.isEmpty()) View.VISIBLE
+        else View.GONE
+
+        adapter.submitList(list)
     }
 
     companion object {
